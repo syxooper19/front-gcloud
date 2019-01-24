64ec6bfd7df426d39d48300b1ef272d42e67b7fd
A82079 <bastien.roussel@enedis.fr>
2019-01-24 16:47:11 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent  {
+
+  itemsBDD : any;
+
+  constructor (http : HttpClient){
+
+    http.get("http://34.76.145.237:3000/personnes")
+    .subscribe((data : any) =>
+      {
+         console.dir(data);
+         this.itemsBDD = data;
+         console.log(this.itemsBDD);
+      },
+      (error : any) =>
+      {
+         console.dir(error);
+      });
+
+
+      
+
+  }
+
+
   title = 'front2-gcloud';
+
+
 }
